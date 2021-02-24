557c9b666d5cc5186d27c54430060cf0b1c9a78f
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-02-24 15:59:17 +0900

    add sample app file

---
+++ src/aws_lambda/aws_lambda/app.py
@@ -0,0 +1,11 @@
+from typing import Any, Dict
+
+from aws_lambda_typing import Context  # type: ignore
+
+LambdaDict = Dict[str, Any]
+
+
+def lambda_handler(event: LambdaDict, context: Context) -> LambdaDict:
+    print("event : ", event)
+    print("context : ", context)
+    return {"hello": "world"}
