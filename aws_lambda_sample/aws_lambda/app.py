a466cc67e6c7bf10660d1566f2be30d3e7a3d735
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-02-24 21:04:13 +0900

    return hello world

--- aws_lambda_sample/aws_lambda/app.py
+++ aws_lambda_sample/aws_lambda/app.py
@@ -5,7 +5,7 @@ from aws_lambda_typing import Context  # type: ignore
 LambdaDict = Dict[str, Any]
 
 
-def lambda_handler(event: LambdaDict, context: Context) -> LambdaDict:
+def lambda_handler(event: LambdaDict, context: Context) -> Any:
     print("event : ", event)
     print("context : ", context)
-    return {"hello": "world"}
+    return "hello world"
