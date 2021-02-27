e7ea60df4ec1c637dbc84f6955bb250fae591d90
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-02-27 21:47:55 +0900

    fix uri

--- client_sample/client_sample/__init__.py
+++ client_sample/client_sample/__init__.py
@@ -4,10 +4,9 @@ import aiohttp
 
 
 async def get_response(host: str, port: int, timeout: float = 1.0) -> Any:
+    uri = "/api/v1/hello"
     async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=timeout)) as session:
-        async with session.post(
-            f"http://{host}:{port}/2015-03-31/functions/function/invocations", data="{}"
-        ) as response:
+        async with session.get(f"http://{host}:{port}{uri}", data="{}") as response:
             status = response.status
             assert status == 200, status
             ret = await response.text()
