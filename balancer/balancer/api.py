f5f68473ad90bfa2b8d2cabfe3ebdf85be39f2f1
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-02-27 21:47:36 +0900

    add lock

--- balancer/balancer/api.py
+++ balancer/balancer/api.py
@@ -1,2 +1,15 @@
-def hello_world() -> str:
-    return "hello"
+import threading
+from typing import Any
+
+import requests
+
+lock = threading.Lock()
+
+
+def hello_world(
+    host: str = "localhost",
+    port: int = 9000,
+) -> Any:
+    with lock:
+        ret = requests.post(f"http://{host}:{port}/2015-03-31/functions/function/invocations", data="{}")
+    return ret.json()
